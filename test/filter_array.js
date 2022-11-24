24df07689501c5d8107072ce426834e7d8635fca
Сергей Субботин <zarodiny@yandex.ru>
2022-11-24 17:32:49 +1000

    jsdoc

--- test/filter_array.js
+++ test/filter_array.js
@@ -1,4 +1,10 @@
-// фильтрует массив и оставляет только те элементы, индекс который четный
+
+/**
+ * фильтрует массив и оставляет только те элементы, индекс который четный
+ * @param {array} arr в виде массива, например inString: [0, 1, 2, 3, 4] или [true, false, true, false, true, false]
+ * @returns {array} возвращает отфильтрованный массив [0, 2, 4] или inString: [true, false, true, false, true, false]
+ */
+
 function filterArray(arr) {
     return arr.filter((item, index) => index % 2 === 0);
 }
